10bdf647bc22007738d5345d6dc08f108290628e
nobody <noreply@github.com>
2020-11-01 21:27:40 +0800

    Fix fetch resources url incorrect

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [2.1.2] - 2020-11-01
+
+### Fixed
+- Fix fetch resources url incorrect
+
 ## [2.1.1] - 2020-10-30
 
 ### Fixed
--- src/uri.ts
+++ src/uri.ts
@@ -1,4 +1,4 @@
-import { createAbsoluteURL, http } from './utils';
+import { createAbsoluteURL, isValidURL, http } from './utils';
 import { css } from './css';
 
 class URI {
@@ -9,6 +9,10 @@ class URI {
     }
 
     const assetURL = createAbsoluteURL(url, baseURL);
+    if (!isValidURL(assetURL)) {
+      return content;
+    }
+
     const response = await http.fetch(assetURL);
     if (typeof response !== 'object' || !Object.prototype.hasOwnProperty.call(response, 'data')) {
       return content;
@@ -17,7 +21,7 @@ class URI {
 
     const contentType = response.headers['content-type'] || '';
     if (contentType === 'text/css') {
-      content = await css.process(Buffer.from(content).toString(), baseURL);
+      content = await css.process(Buffer.from(content).toString(), assetURL);
     }
 
     return content;
