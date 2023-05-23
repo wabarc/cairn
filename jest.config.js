aa316a9fb6f0d4505727acc1f7584949b91ce033
Wayback Archiver <66856220+waybackarchiver@users.noreply.github.com>
2023-05-23 15:25:36 +0000

    Upgrade dependencies (#684)
    * Remove deprecated ts-jest global define
    
    * Node minimum version 16
    
    * Node maximum version 18

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
   moduleFileExtensions: ['ts', 'js'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
